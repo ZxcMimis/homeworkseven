798ab7a72d5f5f8e0aa87dfb3b41711b3880e163
Руслан Івасюк <126970988+ZxcMimis@users.noreply.github.com>
2025-03-21 23:11:59 +0200

    hbs

---
+++ src/js/objects.js
@@ -0,0 +1,22 @@
+export const technologies = [
+    {
+      name: "Компʼютер",
+      price: 60000,
+    },
+    {
+      name: "Телефон",
+      price: 30000,
+    },
+    {
+      name: "Навушники",
+      price: 4000,
+    },
+    {
+      name: "Смарт-годинники",
+      price: 12000,
+    },
+    {
+      name: "Планшет",
+      price: 25000,
+    }
+  ];
---
+++ src/main.js
@@ -0,0 +1,2 @@
+import './js/work'
+import './js/objects'
